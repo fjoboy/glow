b590a74eddc501a5d40b50780e2863ec2a67ec84
Fabian Johnsen <fjo013uib@gmail.com>
2019-02-27 09:33:55 +0100

    added comments, improved grid positioning

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
+let degree = 45, glowSection = document.querySelector("#glow"), intervalId;
 
-window.onload = ()=> {
-    let degree = 170, glow = document.querySelector("#glow");
-    let intervalId = setInterval(()=>{
-        console.log(degree);
-        changeStylesheetRule(glow.styleSheets[0], "body", "background-image", `linear-gradient(${degree}deg, hsla(199, 98%, 48%, 0.70), hsla(187, 100%, 42%, 0.75), hsla(207, 90%, 54%, 0.7), transparent)`);
-        if(degree === 180) degree = -179;
+//increments degree of linear-gradient in intervals of 300ms
+function startGlow(){
+    intervalId = setInterval(() => {
+        changeStylesheetRule(document.styleSheets[0], "#glow", "background-image", `linear-gradient(${degree}deg, hsla(199, 98%, 48%, 0.70), hsla(187, 100%, 42%, 0.75), hsla(207, 90%, 54%, 0.7))`);
+
+        //keep degree within allowed range
+        if (degree === 180) degree = -179;
         else degree++;
-
     }, 300);
-
 }
+
+function stopGlow(){
+    clearInterval(intervalId);
+}
+
 //Add or change the value of properties withing the given stylesheet
 function changeStylesheetRule(stylesheet, selector, property, value) {
     let s = stylesheet;
@@ -17,8 +22,9 @@ function changeStylesheetRule(stylesheet, selector, property, value) {
     property = property.toLowerCase();
     value = value.toLowerCase();
 
-    for(var i = 0; i < s.cssRules.length; i++) {
-        var rule = s.cssRules[i];
+    //for every rule in stylesheet
+    for(let i = 0; i < s.cssRules.length; i++) {
+        let rule = s.cssRules[i];
         if(rule.selectorText === selector) {
             rule.style[property] = value;
             return;
@@ -27,3 +33,5 @@ function changeStylesheetRule(stylesheet, selector, property, value) {
 
     stylesheet.insertRule(selector + " { " + property + ": " + value + "; }", 0);
 }
+
+startGlow();
